5880739486c72b736b3b48b432f5321379dee07e
imrightguy <christopher.maltais@gmail.com>
2026-03-04 06:39:10 -0500

    fix: remove unused import in region capture test

--- test/services/vision/region_capture_service_test.dart
+++ test/services/vision/region_capture_service_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter/services.dart';
 import 'package:cloudtolocalllm/services/vision/region_capture_service.dart';
 
 void main() {
